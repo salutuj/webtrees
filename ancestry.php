3d2b5b2af4834711c2024a00d46574a15ec82f71
Greg Roach <fisharebest@gmail.com>
2017-04-23 22:00:22 +0100

    wip

--- ancestry.php
+++ ancestry.php
@@ -41,37 +41,38 @@ $controller
 	<input type="hidden" name="ged" value="<?= $controller->tree()->getNameHtml() ?>">
 
 	<div class="row form-group">
-		<label class="col-sm-3 col-form-label" for="rootid">
+		<label class="col-sm-3 col-form-label wt-page-options-label" for="rootid">
 			<?= I18N::translate('Individual') ?>
 		</label>
-		<div class="col-sm-9">
+		<div class="col-sm-9 wt-page-options-value">
 			<?= FunctionsEdit::formControlIndividual($controller->root, ['id' => 'rootid', 'name' => 'rootid']) ?>
 		</div>
 	</div>
 
 	<div class="row form-group">
-		<label class="col-sm-3 col-form-label" for="PEDIGREE_GENERATIONS">
+		<label class="col-sm-3 col-form-label wt-page-options-label" for="PEDIGREE_GENERATIONS">
 			<?= I18N::translate('Generations') ?>
 		</label>
-		<div class="col-sm-9">
+		<div class="col-sm-9 wt-page-options-value">
 			<?= Bootstrap4::select(FunctionsEdit::numericOptions(range(2, $controller->tree()->getPreference('MAX_PEDIGREE_GENERATIONS'))), $controller->generations, ['id' => 'PEDIGREE_GENERATIONS', 'name' => 'PEDIGREE_GENERATIONS']) ?>
 		</div>
 	</div>
 
 	<fieldset class="form-group">
 		<div class="row">
-			<legend class="col-form-legend col-sm-3">
+			<legend class="col-form-legend col-sm-3 wt-page-options-label">
 				<?= I18N::translate('Layout') ?>
 			</legend>
-			<div class="col-sm-9">
-				<?= Bootstrap4::radioButtons('chart_style', ['0' => I18N::translate('List'), '1' => I18N::translate('Booklet'), '2' => I18N::translate('Individuals'), '3' => I18N::translate('Families')], $controller->chart_style, true, ['onclick' => 'statusDisable("show_cousins");']) ?>
-				<?= Bootstrap4::checkbox(I18N::translate('Show cousins'), false, ['name' => 'show_cousins']) ?>
+			<div class="col-sm-9 wt-page-options-value">
+				<?= Bootstrap4::radioButtons('chart_style', ['0' => I18N::translate('List'), '1' => I18N::translate('Booklet'), '2' => I18N::translate('Individuals'), '3' => I18N::translate('Families')], $controller->chart_style, true, ['onchange' => '$("input[name=show_cousins]").prop("disabled", $(this).val() !== "1")']) ?>
+				<?= Bootstrap4::checkbox(I18N::translate('Show cousins'), false, ['name' => 'show_cousins', 'disabled' => $controller->chart_style !== '1']) ?>
 			</div>
 		</div>
 	</fieldset>
 
 	<div class="row form-group">
-		<div class="col-sm-9 offset-sm-3">
+		<div class="col-form-label col-sm-3 wt-page-options-label"></div>
+		<div class="col-sm-9 wt-page-options-value">
 			<input class="btn btn-primary" type="submit" value="<?= /* I18N: A button label. */ I18N::translate('view') ?>">
 		</div>
 	</div>
